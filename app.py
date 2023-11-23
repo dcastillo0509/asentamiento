cb478cd3771ccc51db6f4d8a715bb599bfc1eb3a
dcastillo0509 <143881222+dcastillo0509@users.noreply.github.com>
2023-11-22 20:43:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,22 @@ import plotly.graph_objs as go
 
 #Se importa frontend
 from frontend.main import layout
+from frontend.centro.centro import consultar
 
 app=dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 #crea la variable app y agrega __name__ y los estilos con bootstrap
 server=app.server
 app.layout = layout #se asigna el layout en la variable layout de app
+
+@app.callback(
+    [Output('asentamiento', 'figure'),
+     Output('distancia-actual', 'children'),
+     Output('alerta-texto', 'children')],
+    [Input('interval-component', 'n_intervals')]
+)
+
+def consultar_centro(n):
+    return centro.consultar(n)
 
 if __name__ == '__main__': #si __name__ y __main__ son iguales
     app.run_server(debug=True) #Ejecuta la app
